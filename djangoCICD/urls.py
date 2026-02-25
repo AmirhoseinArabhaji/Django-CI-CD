3f9a200412d706d5628b62e1149becaa72718a9b
Amirhosein Arabhaji <amirhosein.arabhaji@gmail.com>
2026-02-25 18:21:56 +0100

    add ping view to urls for health check endpoint

--- djangoCICD/urls.py
+++ djangoCICD/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+def ping_view(request):
+    from django.http import JsonResponse
+    return JsonResponse({'message': 'pong'})
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ping/', ping_view, name='ping'),
 ]
