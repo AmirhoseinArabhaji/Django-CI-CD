7346f4c7a9175ec986ebe14c5066af069164c913
Amirhosein Arabhaji <amirhosein.arabhaji@gmail.com>
2026-02-26 18:27:53 +0100

    configure PostgreSQL database settings and add Docker Compose files for development and production

--- djangoCICD/settings.py
+++ djangoCICD/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangoCICD.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_DB', default='django_db'),
+        'USER': env('POSTGRES_USER', default='django_user'),
+        'PASSWORD': env('POSTGRES_PASSWORD', default='django_password'),
+        'HOST': env('POSTGRES_HOST', default='localhost'),
+        'PORT': env('POSTGRES_PORT', default='5432'),
     }
 }
 
